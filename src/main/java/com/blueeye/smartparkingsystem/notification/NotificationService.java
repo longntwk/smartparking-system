ba0dff0ba66f86dd506ee6ff0ed321f186ba4d9a
longntwk <longntwk@gmail.com>
2025-07-21 17:57:57 +0700

    feat: add notification exit

--- src/main/java/com/blueeye/smartparkingsystem/notification/NotificationService.java
+++ src/main/java/com/blueeye/smartparkingsystem/notification/NotificationService.java
@@ -1,6 +1,7 @@
 package com.blueeye.smartparkingsystem.notification;
 
 import com.blueeye.smartparkingsystem.event.VehicleEnteredEvent;
+import com.blueeye.smartparkingsystem.event.VehicleExitedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,9 @@ public class NotificationService {
     public void notifiOnVehicleEntry(VehicleEnteredEvent event){
         System.out.println("Notification: Vehicle "+ event.vehicleNumber()  +
                 "entered at "+ event.entryTime()  + " Welcome!");
+    }
+    @EventListener
+    public void notifiOnVehicleExit(VehicleExitedEvent event){
+        System.out.println("Notification: Vehicle "+ event.vehicleNumber()+" has exited.");
     }
 }
